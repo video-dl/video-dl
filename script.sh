9de08a9ad0a3955069d1ba4bcbc26d26e396d862
Daniil Gentili <daniil.gentili.dg@gmail.com>
2015-11-27 18:00:34 +0100

    Switched to official dropbox api

--- script.sh
+++ script.sh
@@ -4,12 +4,12 @@ source data
 git remote rm origin
 git remote add origin https://$GH_TOKEN@github.com/video-dl/video-dl
 git push origin :${TRAVIS_BRANCH}
+git submodule init
 clear
-cd ..
-rm -rf video-dl
 mkdir tmp
 cd tmp
-
+export PATH=$PATH:$PWD
+export dir=$PWD
 
 dl() {
 [ "$2" = "" ] && filename="%(title)s.%(ext)s" || filename="$2"
@@ -17,10 +17,7 @@ youtube-dl --newline --output "$filename" "$1" 2>&1 | sed '/\%/!d;s/.*\]\s*//g;s
 }
 
 dropbox() {
-curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
-chmod +x dropbox_uploader.sh
-echo "$1" >../token
-./dropbox_uploader.sh -p -f ../token upload "$2" "$2"
+
 }
 
 
